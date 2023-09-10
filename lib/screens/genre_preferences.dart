734b4de6749983736751bfba8d310c2c1e399ffa
Magr Cruz <maria.cruz@ucsp.edu.pe>
2023-09-10 16:27:49 -0500

    Genre selection with images

--- lib/screens/genre_preferences.dart
+++ lib/screens/genre_preferences.dart
@@ -27,10 +27,19 @@ class _GenrePreferencesState extends State<GenrePreferences> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Selecciona tus géneros favoritos'),
+        title: Text('Preferencias'),
       ),
       body: Column(
         children: <Widget>[
+          ListTile(
+            title: Text(
+              'Selecciona los géneros de tu preferencia',
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: TextField(
@@ -39,13 +48,35 @@ class _GenrePreferencesState extends State<GenrePreferences> {
               decoration: InputDecoration(
                 labelText: 'Buscar género',
                 hintText: 'Escribe aquí...',
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.clear), // Icono para borrar el texto
+                  onPressed: () {
+                    searchController.clear(); // Borra el texto de búsqueda
+                    // Restablece la lista de géneros a su estado original
+                    setState(() {
+                      allGenres = [
+                        'Pop',
+                        'Rock',
+                        'Hip Hop',
+                        'Jazz',
+                        'Electronic',
+                        'Classical',
+                        'Country',
+                        'R&B',
+                        'Reggae',
+                        // Agrega más géneros según tus necesidades
+                      ];
+                    });
+                  },
+                ),
               ),
             ),
           ),
           Expanded(
             child: GridView.builder(
               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3, // Dos columnas
+                crossAxisCount: 3, // Tres columnas
+                childAspectRatio: 1.0, // Asegura que los elementos sean cuadrados
               ),
               itemCount: allGenres.length,
               itemBuilder: (context, index) {
@@ -117,7 +148,7 @@ class _GenrePreferencesState extends State<GenrePreferences> {
 class GenreTile extends StatelessWidget {
   final String genre;
   final bool isSelected;
-  final void Function(bool)? onSelected; // Corregir la firma de onSelected
+  final void Function(bool)? onSelected;
 
   GenreTile({
     required this.genre,
@@ -127,6 +158,7 @@ class GenreTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final imagePath = 'assets/images/defaultMovie.png';
     return InkWell(
       onTap: () {
         if (onSelected != null) {
@@ -134,20 +166,53 @@ class GenreTile extends StatelessWidget {
         }
       },
       child: Card(
-        color: isSelected ? Colors.blue : Colors.white,
         elevation: 4.0,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            // Aquí puedes agregar la imagen correspondiente a cada género
-            Text(genre),
-          ],
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12.0), // Ajusta el radio de la tarjeta según tus necesidades.
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: isSelected
+                ? LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [Color(0xFF9BD5C9), Color(0xFF86A8E7)],
+                  )
+                : null,
+            borderRadius: BorderRadius.circular(12.0), // Ajusta el radio de la tarjeta según tus necesidades.
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ClipRRect(
+                borderRadius: BorderRadius.circular(12.0), // Ajusta el radio de las esquinas de la imagen.
+                child: Image.asset(
+                  imagePath,
+                  width: 80, // Ajusta el tamaño de la imagen según tus necesidades.
+                  height: 80,
+                  errorBuilder: (context, error, stackTrace) {
+                    // En caso de error al cargar la imagen, muestra la imagen por defecto.
+                    return Image.asset(
+                      'assets/images/defaultMovie.png',
+                      width: 80,
+                      height: 80,
+                    );
+                  },
+                ),
+              ),
+              SizedBox(height: 5), // Espacio entre la imagen y el texto
+              Text(
+                genre,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 14,
+                  fontFamily: 'system-ui', // Cambia la fuente a system ui
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
-void main() => runApp(MaterialApp(
-      home: GenrePreferences(),
-    ));
